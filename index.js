73ee8202f84a20461fb768cb8cacfc793735beef
jonschlinkert <jon.schlinkert@sellside.com>
2015-12-22 19:56:31 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+/*!
+ * pkg-store <https://github.com/jonschlinkert/pkg-store>
+ *
+ * Copyright (c) 2015, Jon Schlinkert.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+var Store = require('data-store');
+
+module.exports = function(cwd, prop) {
+  return new Store('package', {cwd: cwd});
+};
+
+
